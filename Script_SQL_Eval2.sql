e3ddce3723818a8a0504a61406a48b74a9d75e02
Man Wax <regameymanuel@gmail.com>
2022-06-16 14:47:47 +0200

    MàJ script SQL

--- Script_SQL_Eval2.sql
+++ Script_SQL_Eval2.sql
@@ -2,28 +2,26 @@ DROP DATABASE IF EXISTS eval2;
 
 CREATE DATABASE eval2;
 
-USE eval2;
-
 CREATE TABLE Client(
-   N_Client COUNTER,
-   NomClient VARCHAR(50),
-   PrenomClient VARCHAR(50),
+   N_Client INT AUTO_INCREMENT,
+   NomClient VARCHAR(50) ,
+   PrenomClient VARCHAR(50) ,
    PRIMARY KEY(N_Client)
 );
 
 CREATE TABLE Commande(
-   N_Commande COUNTER,
+   N_Commande INT AUTO_INCREMENT,
    DateCommande DATE,
-   MontantCommande CURRENCY,
+   MontantCommande DECIMAL(19,4),
    N_Client INT NOT NULL,
    PRIMARY KEY(N_Commande),
    FOREIGN KEY(N_Client) REFERENCES Client(N_Client)
 );
 
 CREATE TABLE Article(
-   N_Article COUNTER,
-   DesignationArticle VARCHAR(50),
-   PUArticle CURRENCY,
+   N_Article INT AUTO_INCREMENT,
+   DesignationArticle VARCHAR(50) ,
+   PUArticle DECIMAL(19,4),
    PRIMARY KEY(N_Article)
 );
 
@@ -36,6 +34,3 @@ CREATE TABLE SeComposeDe(
    FOREIGN KEY(N_Commande) REFERENCES Commande(N_Commande),
    FOREIGN KEY(N_Article) REFERENCES Article(N_Article)
 );
-
-
-End Sub
